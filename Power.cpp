e3feaacce33a57453c56baf9515da8f91f6b6138
Abhisek Das <103781372+dasabhisek668@users.noreply.github.com>
2024-08-07 21:09:23 +0530

    Finding the power  n ^m
    finding the power using recursion

---
+++ Power.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+using namespace std;
+int Power(int n , int p )
+{
+    if(p==0)
+        return 1;
+    if(p == 1)
+        return n;
+    
+    //recursion 
+    int ans = Power(n,p/2); // this means a ^ b can be written as (a^b/2)^2
+
+    // if p is odd
+    if(p & 1)
+        return ans * ans * n;
+    else 
+        return ans * ans ;
+}
+int main()
+{
+    int n , p ;
+    cout << "Enter the number and power ";
+    cin >> n >> p ;
+    cout << "Power : "<< Power(n,p);
+    return 0;
+}
