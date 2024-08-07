b182fd32325e1589b472346e16848c66a23bc773
Abhisek Das <103781372+dasabhisek668@users.noreply.github.com>
2024-08-07 21:05:22 +0530

    LinearSearch using Recursion

---
+++ LinearSearch.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+bool linearSearch(int *arr,int size,int key)
+{
+    if(arr[0] == key)
+        return true;
+    if(size == 0 )
+        return false;
+        
+    return linearSearch(arr+1,size-1,key);
+}
+int main()
+{
+    int arr[5] = {1,2,4,6,7};
+    cout << linearSearch(arr,5,0);
+    return 0;
+}
