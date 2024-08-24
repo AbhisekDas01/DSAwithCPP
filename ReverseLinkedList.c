759ab0eaeeaaa8667f3699dd8f592eb1e3978226
Abhisek Das <dasabhisek668@gmail.com>
2024-08-24 10:13:29 +0530

    Reverse Linked List

--- ReverseLinkedList.c
+++ ReverseLinkedList.c
@@ -7,9 +7,10 @@ struct Node
 {
     int value;
     struct Node *link;
-} *head = NULL, *headrev = NULL;
+} *head = NULL;
 
 void reverse(struct Node *);
+void reverse2(struct Node *,struct Node*);
 void insert(int);
 void display(struct Node *);
 
@@ -23,9 +24,10 @@ int main()
     }
     printf("Before Reversing ");
     display(head);
-    reverse(head);
+    // reverse(head);
+    reverse2(NULL,head);
     printf("\nAfter revesing ");
-    display(headrev);
+    display(head);
     return 0;
 }
 
@@ -53,7 +55,7 @@ void reverse(struct Node *ptr)
     // base case
     if (ptr->link == NULL)
     {
-        headrev = ptr;
+        head = ptr;
         return;
     }
 
@@ -62,6 +64,26 @@ void reverse(struct Node *ptr)
     // back track
     ptr->link->link = ptr;
     ptr->link = NULL;
+}
+
+
+//method 2
+
+void reverse2(struct Node *prev , struct Node *current)
+{
+    //base case
+    if(current == NULL)
+    {
+        head = prev;
+        return;
+    }
+
+    struct Node *next = current->link;
+    current->link = prev;
+
+    //recursion 
+    reverse2(current,next);
+
 }
 
 // Display function
