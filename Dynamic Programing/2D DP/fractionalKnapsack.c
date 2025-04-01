bcfb14332c3274e98cdbc1c9a5ffe2fca802de23
Abhisek Das <dasabhisek668@gmail.com>
2025-04-01 18:10:34 +0530

    Added qsort functionality

--- Dynamic Programing/2D DP/fractionalKnapsack.c
+++ Dynamic Programing/2D DP/fractionalKnapsack.c
@@ -1,83 +1,52 @@
 #include <stdio.h>
 #include <stdlib.h>
+
 typedef struct Item {
     int value;
     int weight;
 } Item;
 
-void swap(Item *a , Item *b){
-    Item temp = *a;
-    *a = *b;
-    *b = temp;
+int compare(const void *a, const void *b) {
+    Item *item1 = (Item *)a;
+    Item *item2 = (Item *)b;
+
+    double val1 = (double)item1->value / (double)item1->weight;
+    double val2 = (double)item2->value / (double)item2->weight;
+
+    if (val1 < val2) return 1;
+    if (val1 > val2) return -1;
+    return 0;
 }
 
-int partition(Item *item , int start , int end){
+double fractionalKnapSack(Item *item, int size, int capacity) {
+    if (capacity == 0 || size == 0) return 0.0;
 
-    double pivot = (double)item[end].value/(double)item[end].weight;
-    int i = start;
+    qsort(item, size, sizeof(Item), compare);
 
-    for(int j = start ; j < end ; j++){
-        
-        double value = (double)item[j].value/(double)item[j].weight;
+    int currentWeight = 0;
+    double finalValue = 0;
 
-        if(value >= pivot){
-            swap(&item[i++] , &item[j]);
-        }
-    }
-
-    swap(&item[i] , &item[end]);
-
-    return i;
-}
-void quickSort(Item *item , int start , int end){
-
-    if(start >= end) return;
-
-    int pivot = partition(item , start , end);
-
-    quickSort(item , start , pivot-1);
-    quickSort(item , pivot+1 , end);
-
-}
-
-//fractional knapsack solution
-double fractionalKnapSack(Item *item , int size , int capacity){
-
-    if(capacity == 0 || size == 0) return 0.0;
-    //sort in decending order
-    quickSort(item , 0 , size-1);
-
-    int currentWeight = 0; //it stores the total weight of taken items in knapsack
-    double finalValue = 0; //it stores the total value of knapsack items
-
-    for(int i = 0 ; i < size ;i++){
-
-        //if the weight of the item is less than the capacity remaining then take it  
-        if(currentWeight + item[i].weight <= capacity){
-
-            currentWeight += item[i].weight; //add the weight
+    for (int i = 0; i < size; i++) {
+        if (currentWeight + item[i].weight <= capacity) {
+            currentWeight += item[i].weight;
             finalValue += item[i].value;
-        }else{
-            //if item weight exceeds the capacity then take the fraction of it
+        } else {
             double remainingCapacity = capacity - currentWeight;
-            double perUnitPrice = (double)item[i].value/(double)item[i].weight;
-
+            double perUnitPrice = (double)item[i].value / (double)item[i].weight;
             double price = remainingCapacity * perUnitPrice;
-
             finalValue += price;
+            break;
         }
     }
 
     return finalValue;
-
 }
 
-int main()
-{
+int main() {
     int size;
 
     printf("Enter the number of elements : ");
-    scanf("%d" , &size);
+    scanf("%d", &size);
 
     Item *item = (Item *)malloc(size * sizeof(Item));
 
@@ -86,22 +55,20 @@ int main()
         return 1;
     }
 
-    printf("Enter %d values : \n",size);
+    printf("Enter %d values : \n", size);
 
-    for(int i = 0 ; i < size ;i++){
-
-        printf("value & weight of %d : " , i+1);
-        scanf("%d%d" , &item[i].value , &item[i].weight);
+    for (int i = 0; i < size; i++) {
+        printf("value & weight of %d : ", i + 1);
+        scanf("%d%d", &item[i].value, &item[i].weight);
     }
 
     int capacity;
     printf("Enter the capacity of knapsack : ");
-    scanf("%d" , &capacity);
+    scanf("%d", &capacity);
 
-    double profit = fractionalKnapSack(item , size , capacity);
+    double profit = fractionalKnapSack(item, size, capacity);
 
-    printf("The profit is : %.2lf" , profit);
-    
+    printf("The profit is : %.2lf", profit);
 
     free(item);
     return 0;
