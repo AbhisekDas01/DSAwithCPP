5d0a43411e8ed3b2616bea298f73d74ff3cf2307
Abhisek Das <dasabhisek668@gmail.com>
2025-06-09 11:37:31 +0530

    Re Implemented Trie

--- Trie/TrieImplementation.cpp
+++ Trie/TrieImplementation.cpp
@@ -1,197 +1,156 @@
 #include <iostream>
+#include <string>
 using namespace std;
 
-//tire node creation
-class TrieNode
-{
+class TrieNode{
     public:
-    char data; //to hold the character value 
-    TrieNode *children[26]; //a tree with 26 child nodes to stores A-Z;
+    char data;
+    TrieNode *children[26];
+    bool isTerminated; //to store the word termination
+    
+    TrieNode(char ch){
 
-    bool isTerminal; //to check the string is ended or not 
-
-    TrieNode(char ch)
-    {
-        data = ch;
-        
-        //mark all the child as null
-        for(int i = 0 ; i < 26 ; i++)
-        {
+        this->data = ch;
+        for(int i = 0 ; i< 26 ; i++){
             children[i] = NULL;
         }
-
-        isTerminal = false;
+        isTerminated = false;
     }
+    
 };
 
-class Trie
-{
-    private:
-    TrieNode *root ;
 
-    public:
-    Trie()
-    {
-        root = new TrieNode('\0');
-    }
+class Trie{
 
     private:
-    //to insert letters one by one recursivly into the tire 
-    void insertUtil(TrieNode *root , string word)
-    {
-        //base case 
-        //if length of the sring becomes 0 then the insertion is complete
-        if(word.length() == 0)
-        {
-            //mark the last node as terminal node
-            root->isTerminal = true;
-            cout << "Inserted..." << endl;
+    TrieNode *root;
+
+
+    //insert util function
+    void insertUtil(TrieNode *root , string word){
+
+        //base case
+        if(word.length() == 0){
+            root->isTerminated = true;
+            cout << "Inserted word.." << endl;
             return;
         }
 
-        //step->1 find index of the child node
-        int idx = word[0] - 'A';
+        //step->1 find the index to insert
+        int idx = word[0] - 'a';
 
-        TrieNode *child; 
+        TrieNode *child;
 
-        //if character already present in the node then it will be not null
-        if(root->children[idx] != NULL)
-        {
+        //if char already present
+        if(root->children[idx]){
+
             child = root->children[idx];
         }
-        else
-        {
-            //if children == NULL then craete new node
+        else{ //if not present
+            // cout << word[0] << endl;
             child = new TrieNode(word[0]);
             root->children[idx] = child;
         }
 
-        //recursive calls for next part 
-        insertUtil(child , word.substr(1)); //pass the remaining string ;
+        insertUtil(child , word.substr(1));
     }
 
-    //search fuction
-    bool searchUtil(TrieNode *root , string word)
-    {
-        //base case
-        //if we moved to the last node and its the terminal node then we found the word
-        if(word.length() == 0)
-        {
-            return root->isTerminal;
+    bool searchUtil(TrieNode* root , string word){
+        // base case 
+        if(word.length() == 0){
+            return root->isTerminated;
         }
 
-        //find index 
-        int idx = word[0] - 'A';
+        int idx = word[0] - 'a';
 
-        TrieNode *child ;
-        //if the letter exists then the child will != NULL
-        if(root->children[idx] != NULL)
-        {
-            //move to forward 
+        TrieNode *child;
+
+        if(root->children[idx]){
             child = root->children[idx];
-        }
-        else
-        {
-            //if is is null then element not present 
+        }else{
             return false;
         }
 
-        //recursive calls 
         return searchUtil(child , word.substr(1));
     }
 
-
-    //to check children
-    bool haveChildren(TrieNode *root)
-    {
-        for(int i = 0 ; i < 26 ; i++)
-        {
-            if(root->children[i] != NULL)
-            {
+    //delete function
+    bool haveChildren(TrieNode *root){
+        for(int i = 0 ; i < 26 ; i++){
+            if(root->children[i])
                 return true;
-            }
         }
         return false;
     }
-    //delete function 
-    bool deleteUtil(TrieNode *root , string word)
-    {
-        //base case 
-        if(word.length() ==  0)
-        {
-            //we are at the last letter so mark if false for terminal node
-            root->isTerminal = false;
+    bool deleteUtil(TrieNode *root , string word){
 
-            //check if it has any other child so it cant be deleted 
+        //base case
+        if(word.length() == 0){
+            root->isTerminated = false;
+
+            //check is children exists
             return !haveChildren(root);
         }
 
-        int idx = word[0] - 'A';
+        int idx = word[0] - 'a';
+
         TrieNode *child = root->children[idx];
 
-        //find if the child  node can be deleted or not 
-        bool canBeRemoved = deleteUtil(child , word.substr(1));
+        bool canDelete = deleteUtil(child , word.substr(1));
 
-        //if it can be removed the delte it
-        if(canBeRemoved)
-        {
+        if(canDelete){
             delete root->children[idx];
             root->children[idx] = NULL;
         }
 
-        //return if root can be deleted or not 
-        //case -1 ->if root is a terminating node 
-        //case -2 -> if root has any other child nodes 
-        //then we cant delete root so return false
-        if(root->isTerminal || haveChildren(root))
-        {
+        //check if root can be deleted
+        if(root->isTerminated || haveChildren(root)){
             return false;
-        }
-        else
-        {
+        }else  
             return true;
-        }
     }
+
+
 
     public:
-    void insertWord(string word)
-    {
-        insertUtil(root , word);
-    }
-    
-    bool searchWord(string word)
-    {
-        return searchUtil(root , word);
+    Trie(){
+        root = new TrieNode('\0');
     }
 
-    void deleteWord(string word)
-    {
-        if(searchWord(word))
-        {
+    //insert word function
+    void insertWord(string word){
+        insertUtil(root , word);
+    }
+
+    //search function 
+    bool searchWord(string word){
+        
+        if(searchUtil(root  , word)){
+            cout << word << " is found" << endl;
+            return true;
+        }
+        cout << word << " is not found!" << endl;
+        return false;
+    }
+
+    void deleteWord(string word){
+
+        if(searchWord(word)){
+
             deleteUtil(root , word);
+            cout << "Deleted " << word << " ! " << endl;
         }
     }
 };
-
-int main()
-{
+int main(){
+    
     Trie t;
 
-    t.insertWord("ABHI");
-    t.insertWord("ABHISEK");
-    t.insertWord("ABH");
-
-    if(t.searchWord("ABH"))
-    {
-        cout << "word found" << endl;
-    }
-    else
-    {
-        cout << "Not found " << endl;
-    }
-    t.deleteWord("ABH");
-    cout << t.searchWord("ABH") << endl;
-    cout << t.searchWord("ABHI");
-
+    t.insertWord("abhisek");
+    t.insertWord("abhipsa");
+    t.insertWord("abhinav");
+    t.searchWord("abhise");
+    t.deleteWord("abhisek");
+    t.searchWord("abhisek");
     return 0;
 }
